5c0cdf9150b32365eb878baa09e88d98829d5647
OshiranCohen <167321902+OmerShiranC@users.noreply.github.com>
2024-07-17 15:19:23 -0700

    clean

--- Car Driving/DQN.py
+++
@@ -1,9 +0,0 @@
-# Machine Learning and Deep Learning
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-class PolicyNetwork(nn.Module):
-        def __init__(self, road_env, carenv, settings, train_mode):
-        self.actions = actions
-        self.road_env = road_env     
